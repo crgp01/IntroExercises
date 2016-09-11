246e6db9366b46f0b78d227a527dacd8fffa6625
Cristina <crivera@thoughtworks.com>
2016-09-11 10:43:21 -0500

    FizzBuzz Exercise
    FizzBuzz is a simple number game where you count, but say "Fizz" and/or
    "Buzz" instead of numbers adhering to certain rules.
    
    Create a FizzBuzz() method that prints out the numbers 1 through 100.
    Instead of numbers divisible by three print "Fizz".
    Instead of numbers divisible by five print "Buzz".
    Instead of numbers divisible by three and five print "FizzBuzz".
    
    Sample Output:
    1
    2
    Fizz
    4
    Buzz
    Fizz
    7
    8
    Fizz
    Buzz
    11
    Fizz
    13
    14
    FizzBuzz

---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,53 @@
+/**
+ * Created by crivera on 11/9/16.
+ */
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+
+        FizzBuzz fb = new FizzBuzz();
+        fb.fizzBuzz();
+
+
+    }
+
+    public void fizzBuzz(){
+
+
+        for (int i=1; i<=100; i++){
+
+            if ((divisibleByThree(i)==0) && (divisibleByFive(i)==0))
+                   {
+                System.out.println("FizzBuzz");
+
+            }
+            else if (divisibleByFive(i)==0){
+                System.out.println("Buzz");
+
+            }
+            else if (divisibleByThree(i)==0)
+            {
+                System.out.println("Fizz");
+            }
+            else {
+
+                System.out.println(i);
+            }
+
+        }
+    }
+
+    public  int divisibleByThree (int number){
+
+        return number % 3;
+
+    }
+
+    public int divisibleByFive(int number){
+
+        return number % 5;
+
+    }
+
+
+}
